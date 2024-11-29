e8d686a9abaa3ea702f3403ad9b9964f0048af3a
Edwin Melara <edwin.melara@incaf.gob.sv>
2024-11-29 09:27:31 -0600

    Componente de Input.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,6 +1,16 @@
+'use client'
+
+import React from "react";
 import Input from '@/components/Input';
 
-const Page = () => {
+export default function Page ()
+{
+  const [email, setEmail] = React.useState('');
+
+  const handlerOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }
+
   return (
     <div className={'relative h-full w-full bg-[url("/images/hero.jpg")] bg-no-repeat bg-center bg-fixed bg-cover'}>
       <div className={'bg-black w-full h-full lg:bg-opacity-50'}>
@@ -11,7 +21,13 @@ const Page = () => {
           <div className={'bg-black bg-opacity-70 p-6 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'}>
             <h2 className={'text-white text-4xl mb-8 font-semibold'}>Sign In</h2>
             <div className={'flex flex-col gap-4'}>
-              <Input />
+              <Input
+                label={'Email'}
+                id={'email'}
+                type={'email'}
+                value={email}
+                onChange={handlerOnChange}
+              />
             </div>
           </div>
         </div>
@@ -19,5 +35,3 @@ const Page = () => {
     </div>
   );
 }
-
-export default Page;
